9ddd08f6cf35b375b8fbed424d432b67aba58338
Woosyun <tjddbs2207@gmail.com>
2025-01-18 22:45:11 +0900

    neovim configuration with typecraft videos(EP1,2,3,5)

---
+++ lua/plugins/lua-config.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({})
+      lspconfig.rust_analyzer.setup{
+        capabilities = capabilities,
+        settings = {
+          ['rust-analyzer'] = {
+            diagnostics = {
+              enable = false;
+            }
+          }
+        }
+      }
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
