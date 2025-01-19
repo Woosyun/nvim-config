e1385e200597ecbb2023b9ba9b362d262fb26b87
Woosyun <tjddbs2207@gmail.com>
2025-01-19 23:27:34 +0900

    keymap J

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -3,3 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+vim.keymap.set('n', 'J', vim.diagnostic.open_float, { noremap = true, silent = true })
+
